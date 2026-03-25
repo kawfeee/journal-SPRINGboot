25ab23dce683fa3dd3968f3f0781c7e143856ba6
kawfeee <kaifalikhan347@gmail.com>
2026-03-25 21:49:46 +0530

    emailSending

---
+++ src/test/java/com/journal/journalApp/service/EmailServiceTests.java
@@ -0,0 +1,17 @@
+package com.journal.journalApp.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EmailServiceTests {
+
+    @Autowired
+    private EmailService emailService;
+
+    @Test
+    void testSendMail(){
+        emailService.sendEmail("kaifgamer303@gmail.com", "Test Subject", "HELLO AAP KAISE HO?");
+    }
+}
